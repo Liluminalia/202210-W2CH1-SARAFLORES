5221a98ede72ed9bbe0778f4f4848787788e36ef
Liluminalia <peloazul4@gmail.com>
2022-10-11 07:35:37 +0200

    updating documents javascript

--- airlines/airlines.js
+++ airlines/airlines.js
@@ -1,67 +1,76 @@
-const flights = [
-  { id: 00, to: "New York", from: "Barcelona", cost: 700, scale: false },
-  { id: 01, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
-  { id: 02, to: "Paris", from: "Barcelona", cost: 210, scale: false },
-  { id: 03, to: "Roma", from: "Barcelona", cost: 150, scale: false },
-  { id: 04, to: "London", from: "Madrid", cost: 200, scale: false },
-  { id: 05, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
-  { id: 06, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
-  { id: 07, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
-  { id: 08, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
-  { id: 09, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
-];
-let username = prompt("Introduzca su nombre de usuario: ");
-console.log("Bienvenid@! " + username);
+const airlines = () => {
+    const flights = [
+        { id: 00, to: 'New York', from: 'Barcelona', cost: 700, scale: false },
+        { id: 01, to: 'Los Angeles', from: 'Madrid', cost: 1100, scale: true },
+        { id: 02, to: 'Paris', from: 'Barcelona', cost: 210, scale: false },
+        { id: 03, to: 'Roma', from: 'Barcelona', cost: 150, scale: false },
+        { id: 04, to: 'London', from: 'Madrid', cost: 200, scale: false },
+        { id: 05, to: 'Madrid', from: 'Barcelona', cost: 90, scale: false },
+        { id: 06, to: 'Tokyo', from: 'Madrid', cost: 1500, scale: true },
+        { id: 07, to: 'Shangai', from: 'Barcelona', cost: 800, scale: true },
+        { id: 08, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
+        { id: 09, to: 'Tel-Aviv', from: 'Madrid', cost: 150, scale: false },
+    ];
+    let username = prompt('Introduzca su nombre de usuario: ');
+    console.log('Bienvenid@! ' + username);
 
-flights.forEach(flight => {
-    if (flight.scale === false){
-        console.log("El vuelo con origen: " + flight.from + ", y destino: " + flight.to + " tiene un coste de: " + flight.cost + "€ y no realiza ninguna escala." )
-    }
-    else{
-        console.log("El vuelo con origen: " + flight.from + ", y destino: " + flight.to + " tiene un coste de: " + flight.cost + "€ y realiza una escala." )
-    }
-})
+    flights.forEach((flight) => {
+        if (flight.scale === false) {
+            console.log(
+                'El vuelo con origen: ' +
+                    flight.from +
+                    ', y destino: ' +
+                    flight.to +
+                    ' tiene un coste de: ' +
+                    flight.cost +
+                    '€ y no realiza ninguna escala.'
+            );
+        } else {
+            console.log(
+                'El vuelo con origen: ' +
+                    flight.from +
+                    ', y destino: ' +
+                    flight.to +
+                    ' tiene un coste de: ' +
+                    flight.cost +
+                    '€ y realiza una escala.'
+            );
+        }
+    });
 
-console.log("**************************************************");
-let sum=0;
-let media;
+    console.log('**************************************************');
+    let sum = 0;
+    let media;
 
-// flights.forEach(flight => {
-//      sum += flight.cost
-// })
+    // flights.forEach(flight => {
+    //      sum += flight.cost
+    // })
 
-flights.map(item => {
-    sum += item.cost
-})
+    flights.map((item) => {
+        sum += item.cost;
+    });
 
-media = sum/flights.length 
+    media = sum / flights.length;
 
+    console.log('El coste medio de los vuelos es: ' + media);
+    console.log('**************************************************');
+    let totalscales = 0;
 
+    flights.forEach((flight) => {
+        if (flight.scale === true) {
+            totalscales += 1;
+        } else {
+        }
+    });
+    console.log('Hay ' + totalscales + ' vuelos con escalas.');
+    console.log('**************************************************');
 
-console.log("El coste medio de los vuelos es: " + media);
-console.log("**************************************************");
-let totalscales=0;
+    cities = 0;
 
-flights.forEach(flight => {
-    if (flight.scale === true){
-        totalscales+=1
-    }else{
-        
-    }
-
-
-})
-console.log("Hay " + totalscales + " vuelos con escalas.");
-console.log("**************************************************");
-
-cities=0;
-
-flights.forEach(flight => {
-    if (flight.id > 4){
-        console.log(flight.to) 
-    }else{
-        
-    }
-
-
-})
+    flights.forEach((flight) => {
+        if (flight.id > 4) {
+            console.log(flight.to);
+        } else {
+        }
+    });
+};
---
+++ index.js
@@ -0,0 +1,11 @@
+import (calculadoraPRO) from './calculadora/calculadoraPRO.js';
+import (pasapalabra) from './pasapalabra/pasapalabra.js';
+import (bingo) from './bingo/bingo.js';
+import (airlines) from './airlines/airlines.js';
+
+const main = async () =>{
+
+}
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('main button').addEventListener('clic', main);
+});
